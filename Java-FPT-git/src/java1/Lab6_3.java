1553a8c67ab7580a008decf304d371974cf0b465
LouisPhan <47730525+LouisPhan@users.noreply.github.com>
2019-10-15 10:59:54 +0700

    Update Lab6_3.java
    class SinhVien

--- Java-FPT-git/src/java1/Lab6_3.java
+++ Java-FPT-git/src/java1/Lab6_3.java
@@ -5,12 +5,91 @@
  */
 package java1;
 
+import java.util.Scanner;
+
 /**
  *
  * @author MyPC
  */
 class SinhVien{
     private String hoTen;
+    private String email;
+    private String phone;
+    private String cmnd;
+
+    public String getHoTen() {
+        return hoTen;
+    }
+
+    public void setHoTen(String hoTen) {
+        this.hoTen = hoTen;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getCmnd() {
+        return cmnd;
+    }
+
+    public void setCmnd(String cmnd) {
+        this.cmnd = cmnd;
+    }
+    
+    void Nhap(){
+        Scanner scan = new Scanner(System.in);
+        
+        System.out.println("*****Nhap thong tinh sinh vien*********");
+        
+//Ho ten sinh vien
+        System.out.println("Nhap ho ten: ");
+        setHoTen(scan.nextLine());
+//Nhap email
+        String checkEmail = "\\w+@\\w+(\\.\\w){1,2}";
+        System.out.println("Nhap email: ");
+        setEmail(scan.nextLine());
+        if(!getEmail().equals(checkEmail)){
+            setEmail(scan.nextLine());
+        }
+        
+//So dien thoai
+        String checkPhone = "(0||+84)[0-9]{9}";
+        System.out.println("Nhap so dien thoai: ");
+        setPhone(scan.nextLine());
+        if(!getPhone().equalsIgnoreCase(checkPhone)){
+            setPhone(scan.nextLine());
+        }
+//CMND
+        String checkID = "[0-9]{12}";
+        System.out.println("Nhap CMND: ");
+        setCmnd(scan.nextLine());
+        while(getCmnd().equalsIgnoreCase(checkID)){
+            setCmnd(scan.nextLine());
+        }
+        System.out.println("========================================");
+    }
+    
+    void Xuat(){
+        System.out.println();
+        System.out.println("*******Thong tin sinh vien**********");
+        System.out.println("Ho va Ten: " + getHoTen());
+        System.out.println("Email: " + getEmail());
+        System.out.println("so dien thoai: "+ getPhone());
+        System.out.println("CMND: " + getCmnd());
+    }
 }
 
 public class Lab6_3 {
